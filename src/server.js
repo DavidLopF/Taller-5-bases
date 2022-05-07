6d3be3251ba39c2e69802a4a68536abc29b14ac0
gitForDavidL <71913304+DavidLopF@users.noreply.github.com>
2022-05-06 21:03:45 -0500

    servidor levatado

---
+++ src/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
+const path = require('path');
+const colors = require('colors');
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || 3000;
+        this.host = process.env.HOST || 'localhost';
+        this.server = require('http').createServer(this.app);
+
+        this.middlewares();
+        //this.routes();
+    }
+
+    middlewares() {
+        this.app.use(cors());
+        this.app.use(logger('dev'));
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
+        this.app.use(express.static(path.join(__dirname, 'public')));
+    }
+    routes() {
+        this.app.use('/', require('./routes/index'));
+        this.app.use('/api/v1/', require('./routes/api/v1'));
+    }
+
+    launcher() {
+        this.server.listen(this.port, this.host, () => {
+            console.log(`Server running on http://${this.host}:${this.port}`.green);
+        });
+    }
+}
+
+module.exports = Server;
