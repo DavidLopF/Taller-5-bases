6dde884f9d38db960b6e73008f8c995066cf7270
Sergio Peña <sergiopena@MacBook-Air-de-Sergio.local>
2022-05-07 16:03:41 -0500

    Create recommendation complete

--- src/controllers/buyer-controller.js
+++ src/controllers/buyer-controller.js
@@ -23,6 +23,49 @@ class BuyerController {
             })
         }
     }
+
+    async recommendProduct(req, res) {
+        try {
+            const { name, product } = req.body
+            console.log(name, product)
+            const recommendation = await this.neo4j.recommendProduct(name, product)
+            
+            res.status(201).json({
+                message: 'Recomendation created successfully',
+                recommendation
+            })
+        } catch (err) {
+            res.status(500).json({
+                message: 'Error creating recommendation',
+                error: err
+            })
+        }
+    }
+
+
+    async getAll(req, res) {
+        try {
+            const buyers = await this.neo4j.getAllBuyer()
+            res.status(200).json({
+                message: 'Buyers retrieved successfully',
+                buyers
+            })
+        } catch (err) {
+            res.status(500).json({
+                message: 'Error retrieving buyers',
+                error: err
+            })
+        }
+    }
+
+    /*async recomendation(req, res) {
+        try {
+            const { name } = req.body
+            const buyer = await this.neo4j.getBuyer(name)
+            const recomendation = await this.neo4j.getRecomendation(buyer)
+            res.status(200).json({
+                message: 'Recomendation successfully created',  
+    */
 }
 
 module.exports = BuyerController    
--- src/routes/buyer-route.js
+++ src/routes/buyer-route.js
@@ -2,11 +2,29 @@ const { Router } = require('express');
 const router = Router();
 const BuyerController = require('../controllers/buyer-controller');
 const buyerController = new BuyerController();
+const { check, validationResult } = require('express-validator');
 
 
 router.route('/')
     .post((req, res) => {
         buyerController.create(req, res);
+    })
+
+router.route('/get_all')
+    .get((req, res) => {
+        buyerController.getAll(req, res);
+    })
+
+router.route('/recommend_product')
+    .post(/*[
+        check('name').isString().withMessage('Name must be a string'),
+        check('name').not().isEmpty().withMessage('Name must not be empty'),
+        check('product').isString().withMessage('Product must be a string'),
+        check('product').not().isEmpty().withMessage('Product must not be empty'),
+        validationResult
+    ],*/
+        (req, res) => {
+        buyerController.recommendProduct(req, res);
     })
 
 module.exports = router;
